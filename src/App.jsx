873882e9dc4e662d7ac6abdf2f0c6ed383907b2b
Lidorc145 <lidorc145@gmail.com>
2025-01-22 16:11:42 +0200

    added reduct-tolkit and tailwind support

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../app/counterSlice'
 
 function App() {
   const [count, setCount] = useState(0)
-
+    const reduxCount = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <div className="card">
+              <h3>useState counter:</h3>
+              <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+              </button>
+          </div><br/>
+          <div><h3>redux counter:</h3>
+              <div>
+                  <button
+                      aria-label="Increment value"
+                      onClick={() => dispatch(increment())}
+                  >
+                      Increment
+                  </button>
+                  <span>{reduxCount}</span>
+                  <button
+                      aria-label="Decrement value"
+                      onClick={() => dispatch(decrement())}
+                  >
+                      Decrement
+                  </button>
+              </div>
+          </div>
+      </>
   )
 }
 
