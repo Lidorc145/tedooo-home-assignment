9bc397e6fde9a176d6b3c265b870136db84d0be6
Lidorc145 <lidorc145@gmail.com>
2025-01-24 00:59:55 +0200

    feed data

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base: '/tedooo-home-assignment',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://backend.tedooo.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
